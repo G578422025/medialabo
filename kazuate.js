0f4c3694b09a3b1e85adccfbd6c060499e6a877d
G578422025 <257842@st.takushoku-u.ac.jp>
2025-06-26 15:52:03 +0900

    kazuate編集

--- kazuate.js
+++ kazuate.js
@@ -1,4 +1,4 @@
-let kotae = Math.floor(Math.random() * 10) + 1; // 正解（1〜10）
+let kotae = Math.floor(Math.random() * 10) + 1; 
 let kaisu = 1;
 
 console.log("答え（デバッグ用）: " + kotae);
@@ -6,7 +6,6 @@ console.log("答え（デバッグ用）: " + kotae);
 function hantei() {
   const yoso = parseInt(document.getElementById("yoso").value);
 
-  // 回数表示を更新
   document.getElementById("kaisu").textContent = kaisu;
 
   let resultMessage = "";
@@ -15,17 +14,16 @@ function hantei() {
     resultMessage = "答えは " + kotae + " でした。ゲームは終了です。";
   } else if (yoso === kotae) {
     resultMessage = "正解です！";
-    kaisu = 4; // 正解したらゲーム終了
+    kaisu = 4;
   } else if (yoso < kotae) {
     resultMessage = "まちがい。答えはもっと大きいよ";
-  } else if (yoso > kotae) {
+  } else {
     resultMessage = "まちがい。答えはもっと小さいよ";
   }
 
   const kaisuText = document.getElementById("kaisu").textContent;
   document.getElementById("answer").textContent = kaisuText + "回目の予想: " + yoso;
 
-  // 結果メッセージを表示
   document.getElementById("result").textContent = resultMessage;
 
   kaisu++;
