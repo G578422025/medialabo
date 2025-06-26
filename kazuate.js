14a0669dff50690759d6100dd5c3d1b08b95e87e
G578422025 <257842@st.takushoku-u.ac.jp>
2025-06-26 15:20:23 +0900

    kazuate編集

--- kazuate.js
+++ kazuate.js
@@ -1,12 +1,6 @@
-let kotae = Math.floor(Math.random() * 10) + 1; // 正解（1〜10のランダム）
-let kaisu = 1;
-
-console.log("答え（デバッグ用）: " + kotae); // コンソールに正解出力
-
 function hantei() {
-  const yoso = parseInt(document.getElementById("yoso").value); // 入力値を整数で取得
+  const yoso = parseInt(document.getElementById("yoso").value);
 
-  // 回数の表示更新
   document.getElementById("kaisu").textContent = kaisu;
 
   let resultMessage = "";
@@ -15,15 +9,17 @@ function hantei() {
   if (kaisu >= 4) {
     resultMessage = "答えは " + kotae + " でした。ゲームは終了です。";
   } else if (yoso === kotae) {
-    answerMessage = "正解です！";
-    kaisu = 4; // 正解したら強制終了扱いに
+    resultMessage = "正解です！";
+    kaisu = 4;
   } else if (yoso < kotae) {
-    answerMessage = "まちがい。答えはもっと大きいよ";
+    resultMessage = "まちがい。答えはもっと大きいよ";
   } else if (yoso > kotae) {
-    answerMessage = "まちがい。答えはもっと小さいよ";
+    resultMessage = "まちがい。答えはもっと小さいよ";
   }
 
-  document.getElementById("answer").textContent = answerMessage;
+  
+  document.getElementById("answer").textContent = kaisu + "回目の予想: " + yoso;
+
   document.getElementById("result").textContent = resultMessage;
 
   kaisu++;
