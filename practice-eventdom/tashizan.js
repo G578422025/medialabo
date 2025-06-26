6884d497d2ddb968d1dbb67a5c7501aa73197467
G578422025 <257842@st.takushoku-u.ac.jp>
2025-06-26 14:16:43 +0900

    tashizan変更

---
+++ practice-eventdom/tashizan.js
@@ -0,0 +1,11 @@
+document.getElementById("calc").addEventListener("click", function() {
+  const left = parseInt(document.getElementById("left").value);
+  const right = parseInt(document.getElementById("right").value);
+
+  if (isNaN(left) || isNaN(right)) {
+    document.getElementById("answer").textContent = "数値を入力してください";
+  } else {
+    const result = left + right;
+    document.getElementById("answer").textContent = result;
+  }
+});
